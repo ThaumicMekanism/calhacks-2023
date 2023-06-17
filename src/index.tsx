134ec7fbca6d76d481a62279681c922d1465a904
Madeline King <madeline.king330@gmail.com>
2023-06-17 12:04:35 -0700

    setup redux files and entry point files

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Provider } from 'react-redux';
+import ReactDOM from "react-dom";
+import Home from './app/page';
+
+ReactDOM.render(
+  <Home />
+, document.getElementById('app'));
