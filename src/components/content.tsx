2626d782e277264fe6e5eed8916ff21b43b751b6
Madeline King <madeline.king330@gmail.com>
2023-06-17 15:19:59 -0700

    add axios methods

---
+++ src/components/content.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from 'react';
+import { useAppSelector, useAppDispatch } from '../store/hooks';
+import { } from '../store/authSlice';
+
+const Content = (): React.JSX.Element => {
+  return (
+    <div>Enter Content Here</div>
+  )
+}
+
+export default Content;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
 import { store } from "../store/store";
 import Navbar from "../components/navbar";
+import Content from "../components/content"
 import "../pages/globals.css";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <Provider store={store}>
       <div>
         <Navbar/>
+        <Content/>
       </div>
     </Provider>
   )
