ce4fe988db756b79c854a2be0dc64872ceb11328
Madeline King <madeline.king330@gmail.com>
2023-06-17 13:11:37 -0700

    change file names

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Provider } from 'react-redux';
-import ReactDOM from "react-dom";
-import Home from './app/page';
-
-ReactDOM.render(
-  <Home />
-, document.getElementById('app'));
