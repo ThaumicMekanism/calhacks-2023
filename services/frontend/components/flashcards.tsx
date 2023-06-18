ae4b11db1ae057b697918652a5d392f336670941
Madeline King <madeline.king330@gmail.com>
2023-06-17 23:57:05 -0700

    render flashcards with dummy data

--- services/frontend/components/flashcards.tsx
+++ services/frontend/components/flashcards.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppSelector, useAppDispatch } from '../store/hooks';
 import { } from '../store/authSlice';
 
 const Flashcards = ({ entry }: any): React.JSX.Element => {
+  const [toggle, setToggle] = useState<boolean | false>(false);
+  const [card, setCard] = useState<string | "front">("front");
 
-  const handleFlip = () => {
+  console.log('this is entry', entry)
 
+  const handleFlip = (): void => {
+    setToggle(!toggle)
+    card === "front" ? setCard("back") : setCard("front");
   }
 
   return (
-    <div className="card-container">
-      <div className="card" onClick={() => handleFlip()} >entry</div>
+    // <div className={toggle ? "card-container" : "card-container flip-container"}>
+    <div>
+      <div className={toggle ? "card" : "card"} onClick={() => handleFlip()} >
+        {card === "front" ? entry.question : entry.answer}
+      </div>
     </div>
+    // </div>
   )
 }
 
