8e4c8ded2569eca6669ee0177037a566fe99f7d8
Dhruv Sangamwar <dsangamwar@gmail.com>
2023-06-18 03:42:24 -0700

    fixed some merge conflicts, working analyze.py

--- services/backend/helper/analyze.py
+++ services/backend/helper/analyze.py
@@ -23,11 +23,6 @@ def make_flash_cards(prompt):
     return qa_list
 
 
-# prompt = "apollo 11 program"
-# completion = make_flash_cards(prompt=prompt)
-
-# def clean_string(input):
-
 if __name__ == "__main__":
     prompt = "The above equation states that the convolution of two signals is equivalent to the multiplication of their Fourier transforms. Therefore, by transforming the input into frequency space, a convolution becomes a single element-wise multiplication. In other words, the input to a convolutional layer and kernel can be converted into frequencies using the Fourier Transform, multiplied once and then converted back using the inverse Fourier Transform. There is an overhead associated with transforming the inputs into the Fourier domain and the inverse Fourier Transform to get responses back to the spatial domain. However, this is offset by the speed up obtained from performing a single multiplication instead of having to multiply the kernel with different sections of the image."
     completion = make_flash_cards(prompt=prompt)
