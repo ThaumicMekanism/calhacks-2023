fdbfa0929782b8d448af4aba149d9baee76ed6cc
John Mema <johnmema@Johns-MacBook-Pro-31.local>
2023-06-17 17:59:55 -0700

    Added some basic pages

--- services/frontend/pages/_app.tsx
+++ services/frontend/pages/_app.tsx
@@ -3,12 +3,44 @@ import { store } from "../store/store";
 import "../pages/globals.css";
 import HomePage from "../pages/homepage"
 import Content from "../pages/ content";
+import { useState } from 'react';
 
 export default function Home() {
+  const [page, setPage] = useState<string | null>(null);
+  function renderBtns() {
+    if (page !== null) return null;
+
+    return (
+      <>
+        <button type='button'onClick={() => setPage("cards")}>
+          cards
+        </button>
+        <button type='button' onClick={() => setPage("home")}>
+          home
+        </button>
+      </>
+    )
+  }
+  function renderCards() {
+    if (page !== "cards") return null;
+
+    return (
+      <Content />
+    )
+  }
+  function renderHome() {
+    if (page !== "home") return null;
+
+    return (
+      <HomePage />
+    )
+  }
   return (
     <Provider store={store}>
       <div>
-        <Content/>
+        {renderBtns()}
+        {renderHome()}
+        {renderCards()}
       </div>
     </Provider>
   )
